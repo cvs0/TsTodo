2ea2dd520767597a6c0b387e882389714d80e6f5
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-10-25 17:49:50 -0600

    Finished.

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -7,16 +7,44 @@ interface item {
 }
 
 export const TodoList: React.FC = () => {
+    const [todos, setTodos] = useState<item[]>([
+        {id: 1, text: 'Learn React', completed: false},
+        {id: 2, text: 'Learn Redux', completed: false},
+    ])
+
+    const [input, setInput] = useState<string>("")
+
+    const handleClick = () => {
+        const newTodo: item = {id: Date.now(), text:input, completed: false}
+        setTodos([...todos, newTodo])
+    }
+
+    const handleToggle = (id: number) => {
+        setTodos(
+            todos.map((todo) => {
+                if(todo.id === id) {
+                    return {...todo, completed:! todo.completed}
+                }
+
+                return todo;
+            })
+        );
+    };
+
     return <div className="main-container">
         <h1>Todo List</h1>
 
         <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
+            {todos.map((todo) => (
+                <li key = {todo.id} onClick={() => handleToggle(todo.id)} style = {{textDecoration: todo.completed ? "line-through" : "none"}}>{todo.text}</li>
+            ))}
         </ul>
 
-        <input type="text" placeholder = "Add todo item" />
-        <button>Add</button>
+        <input
+            type = "text"
+            placeholder = "Add todo item"
+            onChange = {(e) => setInput(e.currentTarget.value)}
+        />
+        <button onClick={handleClick}>Add</button>
     </div>
 }
